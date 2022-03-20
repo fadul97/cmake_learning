c73c90e92bbc0678fd9a8d9b4aa0ecd4c93740f3
Leonardo Fadul <lsffadul@gmail.com>
2022-03-20 10:18:03 -0300

    Example 05 - main in src directory linked static library

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "adder.h"
-
-int main(){
-    std::cout << "Using function from include directory: " << add(4, 4) << std::endl;
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "adder.h"
+
+int main(){
+    std::cout << "Using function from include directory: " << add(0, 5) << std::endl;
+    return 0;
+}
