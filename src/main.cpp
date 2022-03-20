40c5e2add3c9a85ad26f8661d2dd08e16ecc9f4e
Leonardo Fadul <lsffadul@gmail.com>
2022-03-20 10:25:34 -0300

    Example 06 - Shared library

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 #include "adder.h"
 
 int main(){
-    std::cout << "Using function from include directory: " << add(0, 5) << std::endl;
+    std::cout << "Using function from include directory: " << add(0, 6) << std::endl;
     return 0;
 }
